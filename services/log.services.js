8ee67418190ff8fb24153102f48fa07c66cfd4cf
Андрей <andrej@MacBook-Air.local>
2023-01-09 19:36:33 +0300

    feat: implement commant -h

---
+++ services/log.services.js
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (error) => {
+  console.log(chalk.bgRed("ERROR")`${error}`);
+};
+
+const printSuccess = (message) => {
+  console.log(chalk.bgGreen("SUCCESS")`${message}`);
+};
+const printHelp = () => {
+  console.log(
+    dedent(`${chalk.bgCyan("HELP")}    
+	Без параметров  установки погоды 		
+	-s [CITY] для установки города
+	-h для вывода помощи 
+	-t для сохранения токена
+	`)
+  );
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.services.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
   if (args.h) {
+    printHelp();
   }
   if (args.s) {
   }
