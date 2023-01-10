1e350f9c055fea25f161bddb41e0587eacfdd31b
Андрей <andrej@MacBook-Air.local>
2023-01-10 17:20:40 +0300

    feat: implement request into  api use axios

--- services/api.services.js
+++ services/api.services.js
@@ -1,5 +1,5 @@
-import https from "https";
 import { TOKEN_DICTIONARY, getKeyValue } from "./storage.services.js";
+import axios from "axios";
 
 const getWeather = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
@@ -7,18 +7,17 @@ const getWeather = async (city) => {
   if (!token) {
     throw new Error("Не задан ключ API,задайте его через команду -t [API_KEY] ");
   }
-  // const url = ` https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
-  const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("lang", "ru");
-  url.searchParams.append("units", "metric");
 
-  https.get(url, (response) => {
-    let res = "";
-    response.on("data", (data) => (res += data));
-    response.on("end", () => console.log(res));
+  const { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+    params: {
+      q: city,
+      appid: token,
+      lang: "ru",
+      units: "metric",
+    },
   });
+
+  console.log(data);
 };
 
 export { getWeather };
